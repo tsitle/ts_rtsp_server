389cfb9611ff5ac37ad63455af01bd90bc867e2e
tsitle <technisandk@gmail.com>
2026-03-25 19:42:21 +0100

    Add UT NtpTest

---
+++ src/test/java/NtpTest.java
@@ -0,0 +1,137 @@
+import org.junit.jupiter.api.Test;
+import org.tsitle.rtsp.helpers.NtpTimestampHelper;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class NtpTest {
+
+	@Test
+	void ntp_test1() {
+		Instant expInstant = Instant.parse("2023-01-01T00:00:00.000Z");
+		long ntp1 = NtpTimestampHelper.instantToNtpTimestamp(expInstant);
+		Instant actualInstant1 = NtpTimestampHelper.ntpTimestampToInstant(ntp1);
+
+		assertEquals(expInstant, actualInstant1);
+
+		long ntp2 = toNtpTime(expInstant.toEpochMilli());
+
+		assertEquals(ntp1, ntp2);
+
+		long epochMillis = getTime(ntp2);
+		Instant actualInstant2 = Instant.ofEpochMilli(epochMillis);
+
+		assertEquals(expInstant.toEpochMilli(), actualInstant2.toEpochMilli());
+	}
+
+	@Test
+	void ntp_test2() {
+		Instant expInstant = Instant.now();
+		long ntp1 = NtpTimestampHelper.instantToNtpTimestamp(expInstant);
+		Instant actualInstant1 = NtpTimestampHelper.ntpTimestampToInstant(ntp1);
+
+		Duration delta = Duration.between(expInstant, actualInstant1);
+		assertTrue(delta.toMillis() < 1);
+
+		long ntp2 = toNtpTime(expInstant.toEpochMilli());
+
+		long epochMillis = getTime(ntp2);
+		Instant actualInstant2 = Instant.ofEpochMilli(epochMillis);
+
+		long delta2 = Math.abs(expInstant.toEpochMilli() - actualInstant2.toEpochMilli());
+		assertTrue(delta2 < 1000L);
+	}
+
+	@Test
+	void ntp_test3() {
+		Instant expInstant = Instant.parse("2026-01-01T00:00:00.000Z");
+		long ntp1 = NtpTimestampHelper.instantToNtpTimestamp(expInstant);
+		long ntp2 = NtpTimestampHelper.addNanosToNtpTimestamp(ntp1, 1033L);
+		long delta1 = NtpTimestampHelper.diffNanos(ntp1, ntp2);
+		assertEquals(1032L, delta1);  // 1ns less than expected due to a rounding error
+
+		long ntp3 = NtpTimestampHelper.addNanosToNtpTimestamp(ntp2, 45_102_030L);
+		long delta2 = NtpTimestampHelper.diffNanos(ntp1, ntp3);
+		assertEquals(45_102_030L + 1032L, delta2);  // 1ns less than expected due to a rounding error
+
+		Instant actualInstant = NtpTimestampHelper.ntpTimestampToInstant(ntp3);
+		Duration delta = Duration.between(expInstant, actualInstant);
+		assertTrue(delta.toMillis() >= 45 && delta.toMillis() <= 46);
+		assertTrue(delta.toNanos() >= 45_102_030L + 1032L && delta.toNanos() <= 45_102_030L + 1033L);
+	}
+
+	// -----------------------------------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------------------------------
+
+	/**
+	 * Baseline NTP time if bit-0=0 is 7-Feb-2036 @ 06:28:16 UTC
+	 */
+	private static final long msb0baseTime = 2085978496000L;
+	/**
+	 * Baseline NTP time if bit-0=1 is 1-Jan-1900 @ 01:00:00 UTC
+	 */
+	private static final long msb1baseTime = -2208988800000L;
+
+	/**
+	 * Converts Java time to 64-bit NTP time representation.
+	 *
+	 * @param millis Java time
+	 * @return NTP timestamp representation of Java time value.
+	 */
+	private static long toNtpTime(final long millis) {
+		final boolean useBase1 = millis < msb0baseTime; // time < Feb-2036
+		final long baseTimeMillis;
+		if (useBase1) {
+			baseTimeMillis = millis - msb1baseTime; // dates <= Feb-2036
+		} else {
+			// if base0 needed for dates >= Feb-2036
+			baseTimeMillis = millis - msb0baseTime;
+		}
+
+		long seconds = baseTimeMillis / 1000;
+		final long fraction = baseTimeMillis % 1000 * 0x100000000L / 1000;
+
+		if (useBase1) {
+			seconds |= 0x80000000L; // set high-order bit if msb1baseTime 1900 used
+		}
+
+		return seconds << 32 | fraction;
+	}
+
+	/**
+	 * Gets a Java standard time from a 64-bit NTP timestamp.
+	 * <br />
+	 * Note that java time (milliseconds), by definition, has less precision than NTP time (picoseconds) so converting NTP timestamp to Java time and back to NTP
+	 * timestamp loses precision. For example, Tue, Dec 17 2002 09:07:24.810 EST is represented by a single Java-based time value of f22cd1fc8a, but its NTP
+	 * equivalent are all values ranging from c1a9ae1c.cf5c28f5 to c1a9ae1c.cf9db22c.
+	 *
+	 * @param ntpTimeValue the input time
+	 * @return the number of milliseconds since January 1, 1970, 00:00:00 GMT represented by this NTP timestamp value.
+	 */
+	private static long getTime(final long ntpTimeValue) {
+		final long seconds = ntpTimeValue >>> 32 & 0xffffffffL; // high-order 32-bits
+		long fraction = ntpTimeValue & 0xffffffffL; // low-order 32-bits
+
+		// Use round-off on fractional part to preserve going to lower precision
+		fraction = Math.round(1000D * fraction / 0x100000000L);
+
+		/*
+		 * If the most significant bit (MSB) on the seconds field is set we use a different time base. The following text is a quote from RFC-2030 (SNTP v4):
+		 *
+		 * If bit 0 is set, the UTC time is in the range 1968-2036 and UTC time is reckoned from 0h 0m 0s UTC on 1 January 1900. If bit 0 is not set, the time
+		 * is in the range 2036-2104 and UTC time is reckoned from 6h 28m 16s UTC on 7 February 2036.
+		 */
+		final long msb = seconds & 0x80000000L;
+		if (msb == 0) {
+			// use base: 7-Feb-2036 @ 06:28:16 UTC
+			return msb0baseTime + seconds * 1000 + fraction;
+		}
+		// use base: 1-Jan-1900 @ 01:00:00 UTC
+		return msb1baseTime + seconds * 1000 + fraction;
+	}
+
+
+}
