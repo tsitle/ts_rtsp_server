6e1e0919641bf5d2d8c7322d5f31d9b01dbc91f8
tsitle <technisandk@gmail.com>
2026-06-02 11:38:00 +0200

    UTs: Update RtspProtoLowMsgReaderTest

--- src/test/java/org/tsitle/rtsp/rtsp_msgs/RtspProtoLowMsgReaderTest.java
+++ src/test/java/org/tsitle/rtsp/rtsp_msgs/RtspProtoLowMsgReaderTest.java
@@ -51,15 +51,13 @@ public class RtspProtoLowMsgReaderTest {
 			RtspProtoLowMsgRaw msgParsedRaw = reader.readMessage();
 
 			assertTrue(msgParsedRaw.readSuccess);
-			assertEquals(3, msgParsedRaw.headerLines.size());
+			assertEquals(2, msgParsedRaw.headerLines.size());
 
 			final String expLine1 = "CSeq: 8";
 			final String expLine2 = "RTP-Info: url=\"rtsp://example.com/fizzle/audiotrack\";seq=5712;rtptime=934207921";
-			final String expLine3 = "";
 
 			assertEquals(expLine1, msgParsedRaw.headerLines.get(0));
 			assertEquals(expLine2, msgParsedRaw.headerLines.get(1));
-			assertEquals(expLine3, msgParsedRaw.headerLines.get(2));
 
 			System.out.println(msgParsedRaw);
 
@@ -94,16 +92,145 @@ public class RtspProtoLowMsgReaderTest {
 			RtspProtoLowMsgRaw msgParsedRaw = reader.readMessage();
 
 			assertTrue(msgParsedRaw.readSuccess);
-			assertTrue(msgParsedRaw.headerLines.stream().anyMatch(h -> h.startsWith("RTP-Info:")));
-			assertEquals(3, msgParsedRaw.headerLines.size());
+			assertEquals(2, msgParsedRaw.headerLines.size());
 
 			final String expLine1 = "CSeq: 8";
 			final String expLine2 = "RTP-Info: url=\"rtsp://example.com/fizzle/audiotrack\" seq=5712:rtptime=934207921 ,url='rtsp://example.com/fizzle/videotrack' seq=8888:rtptime=8733749";
-			final String expLine3 = "";
 
 			assertEquals(expLine1, msgParsedRaw.headerLines.get(0));
 			assertEquals(expLine2, msgParsedRaw.headerLines.get(1));
-			assertEquals(expLine3, msgParsedRaw.headerLines.get(2));
+
+			System.out.println(msgParsedRaw);
+
+			rw.closeSocket();
+		}
+	}
+
+	@Test
+	void testMsgBody_correctContLen1() throws Exception {
+		try (ServerSocket serverSocket = new ServerSocket(0);
+				Socket tcpSocket = new Socket("127.0.0.1", serverSocket.getLocalPort());
+				Socket peerSocket = serverSocket.accept()) {
+
+			tcpSocket.setSoTimeout(50);  // only for read()
+
+			//
+			TestLogs logMsgIf = new TestLogs();
+			RtxpTcpReadWrite rw = new RtxpTcpReadWrite(tcpSocket);
+
+			//
+			@SuppressWarnings("TextBlockMigration") String msgForSocketStr = "RTSP/2.0 200 OK\r\n" +
+					"Content-Type: xylo\r\n" +
+					"Content-Length: 60\r\n" +
+					"\r\n" +
+					"v=0\r\n" +
+					"o=mhandley 2890844526 IN IP4 126.16.64.4\r\n" +
+					"s=SDP Seminar";  // <-- no CRLF at the end
+			peerSocket.getOutputStream().write(msgForSocketStr.getBytes(StandardCharsets.UTF_8));
+			peerSocket.getOutputStream().flush();
+
+			//
+			RtspProtoLowMsgReader reader = new RtspProtoLowMsgReader(logMsgIf, rw, true);
+			RtspProtoLowMsgRaw msgParsedRaw = reader.readMessage();
+
+			assertTrue(msgParsedRaw.readSuccess);
+			assertEquals(2, msgParsedRaw.headerLines.size());
+
+			final String expLine1 = "Content-Type: xylo";
+			final String expLine2 = "Content-Length: 60";
+			final int expBodyLen = 60;
+
+			assertEquals(expLine1, msgParsedRaw.headerLines.get(0));
+			assertEquals(expLine2, msgParsedRaw.headerLines.get(1));
+			assertEquals(expBodyLen, msgParsedRaw.body.length());
+
+			System.out.println(msgParsedRaw);
+
+			rw.closeSocket();
+		}
+	}
+
+	@Test
+	void testMsgBody_correctContLen2() throws Exception {
+		try (ServerSocket serverSocket = new ServerSocket(0);
+				Socket tcpSocket = new Socket("127.0.0.1", serverSocket.getLocalPort());
+				Socket peerSocket = serverSocket.accept()) {
+
+			tcpSocket.setSoTimeout(50);  // only for read()
+
+			//
+			TestLogs logMsgIf = new TestLogs();
+			RtxpTcpReadWrite rw = new RtxpTcpReadWrite(tcpSocket);
+
+			//
+			@SuppressWarnings("TextBlockMigration") String msgForSocketStr = "RTSP/2.0 200 OK\r\n" +
+					"Content-Type: xylo\r\n" +
+					"Content-Length: 60\r\n" +
+					"\r\n" +
+					"v=0\r\n" +
+					"o=mhandley 2890844526 IN IP4 126.16.64.4\r\n" +
+					"s=SDP Seminar\r\n";  // <-- with CRLF at the end
+			peerSocket.getOutputStream().write(msgForSocketStr.getBytes(StandardCharsets.UTF_8));
+			peerSocket.getOutputStream().flush();
+
+			//
+			RtspProtoLowMsgReader reader = new RtspProtoLowMsgReader(logMsgIf, rw, true);
+			RtspProtoLowMsgRaw msgParsedRaw = reader.readMessage();
+
+			assertTrue(msgParsedRaw.readSuccess);
+			assertEquals(2, msgParsedRaw.headerLines.size());
+
+			final String expLine1 = "Content-Type: xylo";
+			final String expLine2 = "Content-Length: 60";
+			final int expBodyLen = 60;
+
+			assertEquals(expLine1, msgParsedRaw.headerLines.get(0));
+			assertEquals(expLine2, msgParsedRaw.headerLines.get(1));
+			assertEquals(expBodyLen, msgParsedRaw.body.length());
+
+			System.out.println(msgParsedRaw);
+
+			rw.closeSocket();
+		}
+	}
+
+	@Test
+	void testMsgBody_tooShortContLen() throws Exception {
+		try (ServerSocket serverSocket = new ServerSocket(0);
+				Socket tcpSocket = new Socket("127.0.0.1", serverSocket.getLocalPort());
+				Socket peerSocket = serverSocket.accept()) {
+
+			tcpSocket.setSoTimeout(50);  // only for read()
+
+			//
+			TestLogs logMsgIf = new TestLogs();
+			RtxpTcpReadWrite rw = new RtxpTcpReadWrite(tcpSocket);
+
+			//
+			@SuppressWarnings("TextBlockMigration") String msgForSocketStr = "RTSP/2.0 200 OK\r\n" +
+					"Content-Type: xylo\r\n" +
+					"Content-Length: 1000\r\n" +
+					"\r\n" +
+					"v=0\r\n" +
+					"o=mhandley 2890844526 IN IP4 126.16.64.4\r\n" +
+					"s=SDP Seminar";
+			peerSocket.getOutputStream().write(msgForSocketStr.getBytes(StandardCharsets.UTF_8));
+			peerSocket.getOutputStream().flush();
+
+			//
+			RtspProtoLowMsgReader reader = new RtspProtoLowMsgReader(logMsgIf, rw, true);
+			RtspProtoLowMsgRaw msgParsedRaw = reader.readMessage();
+
+			assertTrue(msgParsedRaw.readSuccess);
+			assertEquals(2, msgParsedRaw.headerLines.size());
+
+			final String expLine1 = "Content-Type: xylo";
+			final String expLine2 = "Content-Length: 62";
+			final int expBodyLen = 62;
+
+			assertEquals(expLine1, msgParsedRaw.headerLines.get(0));
+			assertEquals(expLine2, msgParsedRaw.headerLines.get(1));
+			assertEquals(expBodyLen, msgParsedRaw.body.length());
 
 			System.out.println(msgParsedRaw);
 
