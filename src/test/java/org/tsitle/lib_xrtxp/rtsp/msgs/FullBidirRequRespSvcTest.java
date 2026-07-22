da3234df52b7d91b1be2065d9a04d80588b41bac
tsitle <technisandk@gmail.com>
2026-07-22 19:54:56 +0200

    UTs: update FullBidirRequRespSvcTest class

--- src/test/java/org/tsitle/lib_xrtxp/rtsp/msgs/FullBidirRequRespSvcTest.java
+++ src/test/java/org/tsitle/lib_xrtxp/rtsp/msgs/FullBidirRequRespSvcTest.java
@@ -31,7 +31,6 @@ import org.tsitle.lib_xrtxp.rtsp.lowlevel.RtspConnectionPolicy;
 import org.tsitle.lib_xrtxp.rtsp.misctypes.*;
 import org.tsitle.lib_xrtxp.rtsp.sdp.constants.RtspProtoSdpMediaType;
 import org.tsitle.lib_xrtxp.rtsp.sdp.types.RtspProtoSdpDataMediaEntry;
-import org.tsitle.rtsp_server.config.RtspConfigEsSourceType;
 
 import java.io.IOException;
 import java.net.ServerSocket;
@@ -103,7 +102,7 @@ public class FullBidirRequRespSvcTest {
 			if (idEsSource.getIdStr().orElse("-unset-").equals("dummy-stream-source-video")) {
 				return new ElementaryStreamSourceInfo(
 						RtpPacketType.V_H264,
-						RtspConfigEsSourceType.ST_ES_FILE,
+						RtspProtoEsSourceType.ST_ES_FILE,
 						URI.create("file:///dummy-file-video"),
 						(byte)-1,
 						SampleRateEnum.UNKNOWN,
@@ -116,7 +115,7 @@ public class FullBidirRequRespSvcTest {
 			if (idEsSource.getIdStr().orElse("-unset-").equals("dummy-stream-source-audio")) {
 				return new ElementaryStreamSourceInfo(
 						RtpPacketType.A_LINEAR_PCM_S16_441K_MONO,
-						RtspConfigEsSourceType.ST_ES_FILE,
+						RtspProtoEsSourceType.ST_ES_FILE,
 						URI.create("file:///dummy-file-audio"),
 						(byte)2,
 						SampleRateEnum.SR_044100,
