2b7d0eac27eed32bd81622301f34b4f01a66bcd7
tsitle <technisandk@gmail.com>
2026-07-23 20:37:21 +0200

    Add RtspProtoInvalidPbRangeException

---
+++ src/main/java/org/tsitle/lib_xrtxp/rtsp/exceptions/RtspProtoInvalidPbRangeException.java
@@ -0,0 +1,5 @@
+package org.tsitle.lib_xrtxp.rtsp.exceptions;
+
+public class RtspProtoInvalidPbRangeException extends Exception {
+	public RtspProtoInvalidPbRangeException(String msg) { super(msg); }
+}
