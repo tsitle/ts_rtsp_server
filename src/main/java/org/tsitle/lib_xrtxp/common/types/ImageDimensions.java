dc8063cc17efc6d48870e72d2c2bc9ee6aeb5320
tsitle <technisandk@gmail.com>
2026-08-03 17:15:20 +0200

    ImageDimensions: add scaleToFixed()

--- src/main/java/org/tsitle/lib_xrtxp/common/types/ImageDimensions.java
+++ src/main/java/org/tsitle/lib_xrtxp/common/types/ImageDimensions.java
@@ -21,7 +21,7 @@ public record ImageDimensions(int imgWidth, int imgHeight) implements Cloneable 
 	}
 
 	@SuppressWarnings("unused")
-	public @NonNull ImageDimensions scale(int settingMaxDimension) {
+	public @NonNull ImageDimensions scaleWithLimiter(int settingMaxDimension) {
 		if (isEmpty()) {
 			return ImageDimensions.ofEmpty();
 		}
@@ -35,31 +35,35 @@ public record ImageDimensions(int imgWidth, int imgHeight) implements Cloneable 
 			tmpImgHeight -= 1;
 		}
 
+		ImageDimensions resObj = ImageDimensions.of(this);
 		if (settingMaxDimension > 1 && (tmpImgWidth > settingMaxDimension || tmpImgHeight > settingMaxDimension)) {
-			boolean isLandscape = (tmpImgWidth >= tmpImgHeight);
-			double ratio = (double)tmpImgWidth / (double)tmpImgHeight;
-			if (isLandscape) {
-				tmpImgWidth = settingMaxDimension;
-				if ((tmpImgWidth & 1) != 0) {
-					tmpImgWidth -= 1;
-				}
-				tmpImgHeight = (int)((double)tmpImgWidth / ratio);
-				if ((tmpImgHeight & 1) != 0) {
-					tmpImgHeight -= 1;
-				}
-			} else {
-				tmpImgHeight = settingMaxDimension;
-				if ((tmpImgHeight & 1) != 0) {
-					tmpImgHeight -= 1;
-				}
-				tmpImgWidth = (int)((double)tmpImgHeight * ratio);
-				if ((tmpImgWidth & 1) != 0) {
-					tmpImgWidth -= 1;
-				}
-			}
+			resObj = internalScale(this, settingMaxDimension);
 		}
 
-		return new ImageDimensions(tmpImgWidth, tmpImgHeight);
+		return resObj;
+	}
+
+	@SuppressWarnings("unused")
+	public @NonNull ImageDimensions scaleToFixed(int settingDimension) {
+		if (isEmpty() || settingDimension < 2) {
+			return ImageDimensions.ofEmpty();
+		}
+
+		int tmpImgWidth = imgWidth;
+		int tmpImgHeight = imgHeight;
+		if ((tmpImgWidth & 1) != 0) {
+			tmpImgWidth -= 1;
+		}
+		if ((tmpImgHeight & 1) != 0) {
+			tmpImgHeight -= 1;
+		}
+
+		ImageDimensions resObj = ImageDimensions.of(this);
+		if (tmpImgWidth != settingDimension || tmpImgHeight != settingDimension) {
+			resObj = internalScale(this, settingDimension);
+		}
+
+		return resObj;
 	}
 
 	@Override
@@ -74,6 +78,37 @@ public record ImageDimensions(int imgWidth, int imgHeight) implements Cloneable 
 	@Override
 	public @NonNull ImageDimensions clone() {
 		return new ImageDimensions(imgWidth, imgHeight);
+	}
+
+	private static @NonNull ImageDimensions internalScale(@NonNull ImageDimensions inputDims, int outputDim) {
+		int tmpImgWidth = inputDims.imgWidth();
+		int tmpImgHeight = inputDims.imgHeight();
+
+		boolean isLandscape = (tmpImgWidth >= tmpImgHeight);
+		double ratio = (double)tmpImgWidth / (double)tmpImgHeight;
+		if (isLandscape) {
+			tmpImgWidth = outputDim;
+			if ((tmpImgWidth & 1) != 0) {
+				tmpImgWidth -= 1;
+			}
+			tmpImgHeight = (int)((double)tmpImgWidth / ratio);
+			if ((tmpImgHeight & 1) != 0) {
+				tmpImgHeight -= 1;
+			}
+		} else {
+			tmpImgHeight = outputDim;
+			if ((tmpImgHeight & 1) != 0) {
+				tmpImgHeight -= 1;
+			}
+			tmpImgWidth = (int)((double)tmpImgHeight * ratio);
+			if ((tmpImgWidth & 1) != 0) {
+				tmpImgWidth -= 1;
+			}
+		}
+
+		tmpImgWidth = Math.max(tmpImgWidth, 2);
+		tmpImgHeight = Math.max(tmpImgHeight, 2);
+		return new ImageDimensions(tmpImgWidth, tmpImgHeight);
 	}
 
 }
