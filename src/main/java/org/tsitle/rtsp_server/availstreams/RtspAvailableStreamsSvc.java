83dca34bb0a98e1829b934dec07903d198b7d8a0
tsitle <technisandk@gmail.com>
2026-08-10 20:26:49 +0200

    RtspAvailableStreamsSvc: fix internalUpdateFromAsSvcInputData()

--- src/main/java/org/tsitle/rtsp_server/availstreams/RtspAvailableStreamsSvc.java
+++ src/main/java/org/tsitle/rtsp_server/availstreams/RtspAvailableStreamsSvc.java
@@ -11,18 +11,30 @@ import org.tsitle.lib_xrtxp.rtsp.misctypes.RtspProtoElementaryStreamSource;
 import org.tsitle.lib_xrtxp.rtsp.exceptions.RtspProtoIdInputSourceNotFoundException;
 import org.tsitle.lib_xrtxp.rtsp.exceptions.RtspProtoIdEsSourceNotFoundException;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReadWriteLock;
 import java.util.concurrent.locks.ReentrantReadWriteLock;
 
 public final class RtspAvailableStreamsSvc implements RtspProtoAvailableStreamsInterface {
 
-	private final @NonNull Map<@NonNull RtspProtoIdInputSource, @NonNull RtspProtoInputSource> inputSourceMap = new HashMap<>();
-	private final @NonNull Map<@NonNull RtspProtoIdEsSource, @NonNull RtspProtoElementaryStreamSource> esSourceMap = new HashMap<>();
-	private final @NonNull Map<@NonNull RtspProtoIdEsSource, @NonNull RtspProtoEsSourceExpandedInfo> eseiMap = new HashMap<>();
+	private static class AsData {
+		final @NonNull Map<@NonNull RtspProtoIdInputSource, @NonNull RtspProtoInputSource> inputSourceMap = new HashMap<>();
+		final @NonNull Map<@NonNull RtspProtoIdEsSource, @NonNull RtspProtoElementaryStreamSource> esSourceMap = new HashMap<>();
+		final @NonNull Map<@NonNull RtspProtoIdEsSource, @NonNull RtspProtoEsSourceExpandedInfo> eseiMap = new HashMap<>();
+
+		void move(@NonNull AsData src) {
+			this.inputSourceMap.clear();
+			this.inputSourceMap.putAll(src.inputSourceMap);
+			this.esSourceMap.clear();
+			this.esSourceMap.putAll(src.esSourceMap);
+			this.eseiMap.clear();
+			this.eseiMap.putAll(src.eseiMap);
+		}
+	}
+
+	private final @NonNull AsData asDataStaged = new AsData();
+	private final @NonNull AsData asDataCurrent = new AsData();
 
 	private final ReadWriteLock theLock = new ReentrantReadWriteLock();
 	private final Lock theReadLock = theLock.readLock();
@@ -36,6 +48,12 @@ public final class RtspAvailableStreamsSvc implements RtspProtoAvailableStreamsI
 
 	public void updateAvailableStreams(@NonNull RtspAsSvcInputData asSvcInputData) {
 		/* @TODO
+		 * - svc: only data in staging area
+		 * - svc: set flag that the main thread can poll
+		 * - let the main thread do its thing
+		 */
+
+		/* @TODO
 		 * - main: stop all RTSP TCP + PLAY threads for changed ISs
 		 * - main: stop all changed MQs
 		 * - main: wait until all stopped
@@ -44,12 +62,21 @@ public final class RtspAvailableStreamsSvc implements RtspProtoAvailableStreamsI
 		//
 		theWriteLock.lock();
 		try {
-			copyFromAsSvcInputData(asSvcInputData);
+			internalUpdateFromAsSvcInputData(asSvcInputData);
+		} finally {
+			theWriteLock.unlock();
+		}
+
+		//
+		theWriteLock.lock();
+		try {
+			asDataCurrent.move(asDataStaged);
 		} finally {
 			theWriteLock.unlock();
 		}
 
 		/* @TODO
+		 * - svc: copy from staging area
 		 * - main: re-adjust MQ pool and start new thread per MQ
 		 */
 	}
@@ -60,7 +87,7 @@ public final class RtspAvailableStreamsSvc implements RtspProtoAvailableStreamsI
 	public boolean existsInputSourceId(@NonNull RtspProtoIdInputSource idInputSource) {
 		theReadLock.lock();
 		try {
-			return inputSourceMap.containsKey(idInputSource);
+			return asDataCurrent.inputSourceMap.containsKey(idInputSource);
 		} finally {
 			theReadLock.unlock();
 		}
@@ -71,10 +98,10 @@ public final class RtspAvailableStreamsSvc implements RtspProtoAvailableStreamsI
 			throws RtspProtoIdInputSourceNotFoundException {
 		theReadLock.lock();
 		try {
-			if (! inputSourceMap.containsKey(idInputSource)) {
+			if (! asDataCurrent.inputSourceMap.containsKey(idInputSource)) {
 				throw new RtspProtoIdInputSourceNotFoundException(idInputSource.toString());
 			}
-			return inputSourceMap.get(idInputSource);
+			return asDataCurrent.inputSourceMap.get(idInputSource);
 		} finally {
 			theReadLock.unlock();
 		}
@@ -109,19 +136,19 @@ public final class RtspAvailableStreamsSvc implements RtspProtoAvailableStreamsI
 	@Override
 	public @NonNull RtspProtoEsSourceExpandedInfo getElementaryStreamSourceExpInfo(@NonNull RtspProtoIdEsSource idEsSource)
 			throws RtspProtoIdEsSourceNotFoundException {
-		if (! eseiMap.containsKey(idEsSource)) {
+		if (! asDataCurrent.eseiMap.containsKey(idEsSource)) {
 			throw new RtspProtoIdEsSourceNotFoundException(getEsSourceIdForExcMsg(idEsSource));
 		}
-		return eseiMap.get(idEsSource).clone();
+		return asDataCurrent.eseiMap.get(idEsSource).clone();
 	}
 
 	@Override
 	public double computeElementaryStreamSource_virtualFps(@NonNull RtspProtoIdEsSource idEsSource)
 			throws RtspProtoIdEsSourceNotFoundException {
-		if (! eseiMap.containsKey(idEsSource)) {
+		if (! asDataCurrent.eseiMap.containsKey(idEsSource)) {
 			throw new RtspProtoIdEsSourceNotFoundException(getEsSourceIdForExcMsg(idEsSource));
 		}
-		RtspProtoEsSourceExpandedInfo esei = eseiMap.get(idEsSource);
+		RtspProtoEsSourceExpandedInfo esei = asDataCurrent.eseiMap.get(idEsSource);
 		if (esei.audioSampleRate() == SampleRateEnum.UNKNOWN) {
 			throw new RtspProtoIdEsSourceNotFoundException(getEsSourceIdForExcMsg(idEsSource) +
 					": audioSamplerateHz must be valid");
@@ -151,44 +178,76 @@ public final class RtspAvailableStreamsSvc implements RtspProtoAvailableStreamsI
 	@Override
 	public int getElementaryStreamSource_samplesPerFrame(@NonNull RtspProtoIdEsSource idEsSource)
 			throws RtspProtoIdEsSourceNotFoundException {
-		if (! eseiMap.containsKey(idEsSource)) {
+		if (! asDataCurrent.eseiMap.containsKey(idEsSource)) {
 			throw new RtspProtoIdEsSourceNotFoundException(getEsSourceIdForExcMsg(idEsSource));
 		}
-		RtspProtoEsSourceExpandedInfo esei = eseiMap.get(idEsSource);
+		RtspProtoEsSourceExpandedInfo esei = asDataCurrent.eseiMap.get(idEsSource);
 		return esei.audioSamplesPerFrame();
 	}
 
 	// -----------------------------------------------------------------------------------------------------------------
 	// -----------------------------------------------------------------------------------------------------------------
 
-	private void copyFromAsSvcInputData(@NonNull RtspAsSvcInputData asSvcInputData) {
-		inputSourceMap.clear();
+	private void internalUpdateFromAsSvcInputData(@NonNull RtspAsSvcInputData asSvcInputData) {
+		for (RtspProtoIdInputSource idIs : asSvcInputData.isIdsDeleted) {
+			asDataStaged.inputSourceMap.remove(idIs);
+		}
+
+		//
 		for (Map.Entry<@NonNull RtspProtoIdInputSource, @NonNull RtspProtoInputSource> entry
 				: asSvcInputData.mapIsIdToIsObj.entrySet()) {
-			inputSourceMap.put(
+			asDataStaged.inputSourceMap.put(
 					RtspProtoIdInputSource.of(entry.getKey().getIdStr().orElseThrow()),
 					entry.getValue().clone()
 				);
 		}
 
 		//
-		esSourceMap.clear();
 		for (Map.Entry<@NonNull RtspProtoIdEsSource, @NonNull RtspProtoElementaryStreamSource> entry
 				: asSvcInputData.mapEsIdToEsObj.entrySet()) {
-			esSourceMap.put(
+			asDataStaged.esSourceMap.put(
 					RtspProtoIdEsSource.of(entry.getKey().getIdStr().orElseThrow()),
 					entry.getValue().clone()
 				);
 		}
 
 		//
-		eseiMap.clear();
+		asDataStaged.eseiMap.clear();
 		for (Map.Entry<@NonNull RtspProtoIdEsSource, @NonNull RtspProtoEsSourceExpandedInfo> entry
 				: asSvcInputData.mapEsIdToEseiObj.entrySet()) {
-			eseiMap.put(
+			asDataStaged.eseiMap.put(
 					RtspProtoIdEsSource.of(entry.getKey().getIdStr().orElseThrow()),
 					entry.getValue().clone()
 				);
+		}
+
+		//
+		Set<RtspProtoIdEsSource> esSourceIdsInUse = new HashSet<>();
+		for (Map.Entry<@NonNull RtspProtoIdInputSource, @NonNull RtspProtoInputSource> entry
+				: asDataStaged.inputSourceMap.entrySet()) {
+			esSourceIdsInUse.addAll(entry.getValue().getEsSourceIds());
+		}
+
+		//
+		Set<RtspProtoIdEsSource> esSourceIdsToDelete = new HashSet<>();
+		for (RtspProtoIdEsSource esSourceId : asDataStaged.esSourceMap.keySet()) {
+			if (! esSourceIdsInUse.contains(esSourceId)) {
+				esSourceIdsToDelete.add(esSourceId);
+			}
+		}
+		for (RtspProtoIdEsSource esSourceId : esSourceIdsToDelete) {
+			asDataStaged.esSourceMap.remove(esSourceId);
+		}
+
+		//
+		esSourceIdsToDelete.clear();
+		for (RtspProtoIdEsSource esSourceId : asDataStaged.eseiMap.keySet()) {
+			if (! esSourceIdsInUse.contains(esSourceId)) {
+				esSourceIdsToDelete.add(esSourceId);
+			}
+		}
+		for (RtspProtoIdEsSource esSourceId : esSourceIdsToDelete) {
+			asDataStaged.eseiMap.remove(esSourceId);
 		}
 	}
 
@@ -211,18 +270,18 @@ public final class RtspAvailableStreamsSvc implements RtspProtoAvailableStreamsI
 			return Optional.empty();
 		}
 		for (RtspProtoIdEsSource tmpEsId : tmpInputSource.getEsSourceIds()) {
-			if (! esSourceMap.containsKey(tmpEsId)) {
+			if (! asDataCurrent.esSourceMap.containsKey(tmpEsId)) {
 				continue;  // this should never happen
 			}
-			if (! esSourceMap.get(tmpEsId).getEnabled()) {
+			if (! asDataCurrent.esSourceMap.get(tmpEsId).getEnabled()) {
 				continue;
 			}
-			if (! eseiMap.containsKey(tmpEsId)) {
+			if (! asDataCurrent.eseiMap.containsKey(tmpEsId)) {
 				continue;
 			}
-			RtspProtoEsSourceExpandedInfo esei = eseiMap.get(tmpEsId);
+			RtspProtoEsSourceExpandedInfo esei = asDataCurrent.eseiMap.get(tmpEsId);
 			if ((isVideo && esei.codec().isVideo()) || (! isVideo && esei.codec().isAudio())) {
-				return Optional.of(esSourceMap.get(tmpEsId));
+				return Optional.of(asDataCurrent.esSourceMap.get(tmpEsId));
 			}
 		}
 		return Optional.empty();
