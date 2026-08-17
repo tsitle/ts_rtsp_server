58e2adf275d463e582e65ea08a5231ef570714a2
tsitle <technisandk@gmail.com>
2026-08-17 13:25:32 +0200

    RtspServerConstants: fix default RTSPS TCP port

--- src/main/java/org/tsitle/rtsp_server/threads/rtsp_tcp/RtspServerConstants.java
+++ src/main/java/org/tsitle/rtsp_server/threads/rtsp_tcp/RtspServerConstants.java
@@ -12,7 +12,7 @@ public final class RtspServerConstants {
 	/** Default TCP port for a RTSP server (without SSL/TLS) */
 	public static final int SERVER_RTSP_TCP_PORT = 554;
 	/** Default TCP port for a RTSPS server (with SSL/TLS) */
-	public static final int SERVER_RTSPS_TCP_PORT = 332;
+	public static final int SERVER_RTSPS_TCP_PORT = 322;
 
 	public static final String SERVER_NAME = "TS RTSP Server";
 
